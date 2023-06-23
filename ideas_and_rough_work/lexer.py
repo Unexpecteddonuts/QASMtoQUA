2516f7d76a44227c8a17ef533f60ab0f93607277
Aakif Rehman <aakif@durham.ac.uk>
2023-06-23 23:23:19 +0100

    Update lexer.py

--- ideas_and_rough_work/lexer.py
+++ ideas_and_rough_work/lexer.py
@@ -1,29 +1,31 @@
-import re
+# This code has been decrepated and a new code with the PLY Lexer will be implemented
+# import re
 
-class Lexer:
-    def __init__(self):
-        self.token_patterns = [
-            ("COMMENT", r"//.*"),
-            ("KEYWORD", r"(OPENQASM|include|qreg|creg|gate|measure|reset|barrier|if|else|elif|opaque|unitary)"),
-            ("IDENTIFIER", r"[a-zA-Z_][a-zA-Z0-9_]*"),
-            ("NUMBER", r"\d+(\.\d+)?"),
-            ("STRING", r"\".*\""),
-            ("SYMBOL", r"[\[\]\{\}\(\),;+\-*/^=]"),
-            ("WHITESPACE", r"\s+")
-        ]
+# class Lexer:
+#     def __init__(self):
+#         self.token_patterns = [
+#             ("COMMENT", r"//.*"),
+#             ("KEYWORD", r"(OPENQASM|include|qreg|creg|gate|measure|reset|barrier|if|else|elif|opaque|unitary)"),
+#             ("IDENTIFIER", r"[a-zA-Z_][a-zA-Z0-9_]*"),
+#             ("NUMBER", r"\d+(\.\d+)?"),
+#             ("STRING", r"\".*\""),
+#             ("SYMBOL", r"[\[\]\{\}\(\),;+\-*/^=]"),
+#             ("WHITESPACE", r"\s+")
+#         ]
     
-    def tokenize(self, code):
-        tokens = []
-        while code:
-            match = None
-            for token_name, pattern in self.token_patterns:
-                regex = re.compile(pattern)
-                match = regex.match(code)
-                if match:
-                    value = match.group(0)
-                    tokens.append((token_name, value))
-                    code = code[len(value):]
-                    break
-            if not match:
-                raise ValueError("Invalid token: " + code)
-        return tokens
+#     def tokenize(self, code):
+#         tokens = []
+#         while code:
+#             match = None
+#             for token_name, pattern in self.token_patterns:
+#                 regex = re.compile(pattern)
+#                 match = regex.match(code)
+#                 if match:
+#                     value = match.group(0)
+#                     tokens.append((token_name, value))
+#                     code = code[len(value):]
+#                     break
+#             if not match:
+#                 raise ValueError("Invalid token: " + code)
+#         return tokens
+
